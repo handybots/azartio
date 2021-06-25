4d828d09ef33a899f70a4d548443b9903653c564
Demian <d@aletheia.icu>
2021-06-25 23:15:28 +0300

    clickhouse: update

--- Makefile
+++ Makefile
@@ -2,10 +2,15 @@ include .env
 export
 
 run:
-	go run ./cmd/bot 
-	
+	go run ./cmd/bot
+
 compose:
 	docker-compose up -d
 
 migrate:
 	docker-compose up --force-recreate migrate
+
+deploy-arm:
+	docker buildx build --platform linux/amd64 -t docker.pkg.github.com/handybots/azartio/bot:$$(git rev-parse --short HEAD) .
+	docker push docker.pkg.github.com/handybots/azartio/bot:$$(git rev-parse --short HEAD);
+
--- storage/migrate/clickhouse/001_schema.sql
+++ storage/migrate/clickhouse/001_schema.sql
@@ -1,10 +1,11 @@
-CREATE DATABASE IF NOT EXISTS bot; -- use your name
+CREATE DATABASE IF NOT EXISTS azartio;
 
-CREATE TABLE IF NOT EXISTS bot.logs (
+CREATE TABLE IF NOT EXISTS azartio.logs (
     date        Date,
     time        DateTime,
     level       String,
     message     String,
     event       String,
-    user_id     UInt32
+    user_id     UInt32,
+	chat_id		UInt32
 ) ENGINE = MergeTree(date, (level, event, user_id), 8192);
